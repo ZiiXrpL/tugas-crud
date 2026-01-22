3ef593015530fc831546eeff96b71591bdb85590
greey <ahmadfauzikencong@gmail.com>
2026-01-22 08:03:37 +0700

    first commit

---
+++ app/Models/Order.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'customer',
+        'totalorder',
+        'products',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Edit a Product</h1>
+    <div>
+        @if($errors->any())
+            <div>
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </div>
+    <form method="post" action="{{ route('product.update',['product'=>$product]) }}">
+        @csrf
+        @method('put')
+        <div>
+            <label>Product</label>
+            <input type="text" name="name" placeholder="name" value="{{ $product->name }}">
+        </div>
+        <div>
+            <label>Qty</label>
+            <input type="text" name="qty" placeholder="qty" value="{{ $product->qty }}">
+        </div>
+        <div>
+            <label>Price</label>
+            <input type="text" name="price" placeholder="price" value="{{ $product->price }}">
+        </div>
+        <div>
+            <label>Description</label>
+            <input type="text" name="description" placeholder="description" value="{{ $product->description }}">
+        </div>
+        <input type="submit" value="Update">
+    </form>
+</html>
